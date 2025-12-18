618eb25aedd2e25bac525be3ae44d4f9e11d2e04
Bindu1504 <118755492+Bindu1504@users.noreply.github.com>
2025-12-18 18:59:58 +0530

    Create App.jsx

---
+++ App.jsx
@@ -0,0 +1,18 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="landing-page">
+      <div className="content">
+        <h1>Paradise Nursery</h1>
+        <p>Your one-stop destination for healthy and beautiful plants</p>
+
+        <button className="get-started-btn">
+          Get Started
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
